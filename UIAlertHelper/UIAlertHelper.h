60a09c681d4a52d66e2d39c8b91bb85f7b2202e9
Tran Van Thanh <thanhtran.hce@gmail.com>
2018-09-15 19:06:04 +0700

    Initial Commit

---
+++ UIAlertHelper/UIAlertHelper.h
@@ -0,0 +1,19 @@
+//
+//  UIAlertHelper.h
+//  UIAlertHelper
+//
+//  Created by Sunflower on 9/15/18.
+//  Copyright © 2018 Sunflower. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIAlertHelper.
+FOUNDATION_EXPORT double UIAlertHelperVersionNumber;
+
+//! Project version string for UIAlertHelper.
+FOUNDATION_EXPORT const unsigned char UIAlertHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIAlertHelper/PublicHeader.h>
+
+
