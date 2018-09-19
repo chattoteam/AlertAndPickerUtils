7e3dfa6f7913c1da3f3c2e98231d21014287710a
Tran Van Thanh <thanhtran.hce@gmail.com>
2018-09-19 16:13:43 +0700

    Change the new project sou

---
+++ AlertAndPickerUtils/AlertAndPickerUtils.h
@@ -0,0 +1,19 @@
+//
+//  AlertAndPickerUtils.h
+//  AlertAndPickerUtils
+//
+//  Created by Thanh Tran Van on 9/19/18.
+//  Copyright © 2018 Thanh Tran Van. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertAndPickerUtils.
+FOUNDATION_EXPORT double AlertAndPickerUtilsVersionNumber;
+
+//! Project version string for AlertAndPickerUtils.
+FOUNDATION_EXPORT const unsigned char AlertAndPickerUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertAndPickerUtils/PublicHeader.h>
+
+
